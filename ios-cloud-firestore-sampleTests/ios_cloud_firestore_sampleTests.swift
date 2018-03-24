1bc6c28e3c6ba9ddf0fbaeff420918f6e07eb045
yuu ogasawara <y.ogasawara@st-ventures.jp>
2018-03-24 14:49:02 +0900

    Initial Commit

---
+++ ios-cloud-firestore-sampleTests/ios_cloud_firestore_sampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  ios_cloud_firestore_sampleTests.swift
+//  ios-cloud-firestore-sampleTests
+//
+//  Created by yogasawara@stv on 2018/03/24.
+//  Copyright © 2018年 sunday carpenter. All rights reserved.
+//
+
+import XCTest
+@testable import ios_cloud_firestore_sample
+
+class ios_cloud_firestore_sampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
